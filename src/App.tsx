59de71d9a06346aa7ce985263b4e252506b7aa2c
AnaisRevellat <anaisrevellat@gmail.com>
2022-12-22 10:01:39 +0100

    feature/navbar--navbar-v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import Form from "./components/forms/Form";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const [signup, setSignup] = useState(false);
@@ -11,12 +13,17 @@ function App() {
 
   return (
     <div>
-      <Form
+      <Routes>
+        <Route path='/' element={<NavBar />} />
+      </Routes>
+
+      {/* <Form
         login={signup === false ? "Connectez vous !" : "Créez votre compte"}
         bouttonLabel={signup === false ? "Connexion" : "Créer le compte"}
         onToggle={handleToggleSignup}
         signup={signup}
-      />
+      /> */}
+     
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import ConnexionBtn from "../ConnexionBtn/ConnexionBtn";
+import Logo from "../Logo/Logo";
+import "./navBar.css";
+
+function NavBar() {
+	const [toggleMenu, setToggleMenu] = useState(true);
+	const [screenXWidth, setScreenXWidth] = useState(window.innerWidth);
+
+	const toggleSmallDevice = () => {
+		setToggleMenu(!toggleMenu);
+	};
+
+	useEffect(() => {
+		const changeWidth = () => {
+			setScreenXWidth(window.innerWidth);
+
+			if (window.innerWidth > 1060) {
+				setToggleMenu(false);
+			}
+		};
+
+		window.addEventListener("resize", changeWidth);
+
+		return () => {
+			//to avoid to listen to for nothing
+			window.removeEventListener("resize", changeWidth);
+		};
+	}, []);
+
+	return (
+		<nav className='navBar'>
+			{(toggleMenu || screenXWidth > 1060) && (
+				<div className='mainNavBox'>
+					<Logo />
+
+					<ul className='navList'>
+						<li className='items'>Accueil</li>
+						<li className='items'>Villes</li>
+						<li className='items'>Contact</li>
+					</ul>
+					
+					<ConnexionBtn />
+				</div>
+			)}
+
+			<button onClick={toggleSmallDevice} className='burgerCross'>
+				X
+			</button>
+		</nav>
+	);
+}
+
+export default NavBar;
