02149b1702f8f707bdf7ffd4f7ec9c94903cde66
AnaisRevellat <anaisrevellat@gmail.com>
2023-02-28 14:29:54 +0100

    logic added a new to ModalAuth

--- src/components/ModalAuth/ModalAuth.tsx
+++ src/components/ModalAuth/ModalAuth.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { useState, ReactNode } from "react";
 import "./modalAuth.scss";
 import ReactDOM from "react-dom";
 import { createUser } from "../../graphql/createUser";
@@ -11,6 +11,31 @@ export interface IProps {
 }
 
 export const ModalAuth = ({ open, children, onClose, ...props }: IProps) => {
+
+  const [authMode, setAuthMode] = useState("signin");
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [handleSignupMutation, { data, loading, error }] =
+    useMutation(createUser);
+
+  const handleSignup = async () => {
+    try {
+      await handleSignupMutation({
+        variables: {
+          data: { email, password },
+        },
+      });
+      setEmail("");
+      setPassword("");
+    } catch {}
+  };
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
+
 	if (!open) return null;
 
 	return ReactDOM.createPortal(
@@ -18,13 +43,53 @@ export const ModalAuth = ({ open, children, onClose, ...props }: IProps) => {
 			<div className='overlay_styles' />
 
 			<div className='modal_styles'>
-				{children}
-				hello
-				<div className='redtest'>
-					<div className='modalAuth-component'>
+				{/* {children}				 */}
+
+        <div className="modalAuth-component">
+      <div className={`modalAuth-body`}>
+        <div className="modalAuth__title">S'inscrire</div>
+        <div className="modalAuth__mode">
+          Déjà enregistré ? <span onClick={changeAuthMode}>Se connecter</span>
+        </div>
+        <div className="modalAuth__input">
+          <div className="modalAuth__signin-input">
+            <label htmlFor="email">Email</label>
+            <input
+              disabled={loading}
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              disabled={loading}
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="modalAuth__submit">
+            <button onClick={handleSignup}>Créer un compte</button>
+            <button onClick={onClose}>Close Modal</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+        
+					{/* <div className='modalAuth-component'>
 						<div className='modalAuth-body'>
-							<div className='modalAuth__title'>Se connecter</div>
               <button onClick={onClose}>Close Modal</button>
+							<div className='modalAuth__title'>Se connecter</div>
 							<div className='modalAuth__mode'>
 								Pas de compte ? <span>S'inscrire</span>
 							</div>
@@ -43,8 +108,8 @@ export const ModalAuth = ({ open, children, onClose, ...props }: IProps) => {
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
+					</div> */}
+				
 			</div>
 		</>,
 		document.querySelector("#portal") as Element
