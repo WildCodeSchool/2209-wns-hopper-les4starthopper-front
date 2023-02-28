dd9e1d9af90c5400a326e8f9c763ba600d4765ca
AnaisRevellat <anaisrevellat@gmail.com>
2023-02-28 16:40:32 +0100

    portal and portal2 se

--- src/components/ModalAuth/ModalAuth.tsx
+++ src/components/ModalAuth/ModalAuth.tsx
@@ -35,20 +35,81 @@ export const ModalAuth = ({ open, onClose, ...props }: IProps) => {
 	};
 
 	if (!open) return null;
+	if (authMode === "signin") {
+		return ReactDOM.createPortal(
+			<>
+				<div className='overlay_styles' />
+
+				<div className='modalAuth-component'>
+					<div className={`modalAuth-body`}>
+						<div className='close'>
+							<button onClick={onClose}>X</button>
+						</div>
+						<div className='modalAuth__title'>S'inscrire</div>
+
+						<div className='modalAuth__input'>
+							<div className='modalAuth__signin-input'>
+								<label className='modalAuth__label' htmlFor='email'>
+									Email
+								</label>
+								<input
+									disabled={loading}
+									type='email'
+									name='email'
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value);
+									}}
+								/>
+								<label
+									className='modalAuth__label'
+									htmlFor='password'
+								>
+									Mot de passe
+								</label>
+								<input
+									disabled={loading}
+									type='password'
+									name='password'
+									value={password}
+									onChange={(e) => {
+										setPassword(e.target.value);
+									}}
+								/>
+							</div>
+							<div className='modalAuth__submit'>
+								<button onClick={handleSignup}>
+									Créer un compte
+								</button>
+							</div>
+							<div className='modalAuth__mode'>
+								Déjà enregistré ?{" "}
+								<span
+									className='modalAuth__span'
+									onClick={changeAuthMode}
+								>
+									Se connecter
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>,
+			document.querySelector("#portal") as Element
+		);
+	}
 
 	return ReactDOM.createPortal(
 		<>
 			<div className='overlay_styles' />
 
-			{/* {children}				 */}
-
 			<div className='modalAuth-component'>
 				<div className={`modalAuth-body`}>
 					<div className='close'>
-					<button onClick={onClose} >X</button>
+						<button onClick={onClose}>X</button>
 					</div>
-					<div className='modalAuth__title'>S'inscrire</div>
-		
+					<div className='modalAuth__title'>Se connecter</div>
+
 					<div className='modalAuth__input'>
 						<div className='modalAuth__signin-input'>
 							<label className='modalAuth__label' htmlFor='email'>
@@ -80,18 +141,57 @@ export const ModalAuth = ({ open, onClose, ...props }: IProps) => {
 							<button onClick={handleSignup}>Créer un compte</button>
 						</div>
 						<div className='modalAuth__mode'>
-						Déjà enregistré ?{" "}
-						<span
-							className='modalAuth__span'
-							onClick={changeAuthMode}
-						>
-							Se connecter
-						</span>
-					</div>
+							Déjà enregistré ?{" "}
+							<span
+								className='modalAuth__span'
+								onClick={changeAuthMode}
+							>
+								Se connecter
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
 		</>,
-		document.querySelector("#portal") as Element
+		document.querySelector("#portal2") as Element
 	);
 };
+
+// <div className="modalAuth-component">
+// <div className={`modalAuth-body`}>
+// 	<div className="modalAuth__title">Se connecter</div>
+// 	<div className="modalAuth__mode">
+// 		Pas de compte ? <span onClick={changeAuthMode}>S'inscrire</span>
+// 	</div>
+// 	<div className="modalAuth__input">
+// 		<div className="modalAuth__signup-input">
+// 			<label htmlFor="email">Email</label>
+// 			<input
+// 				disabled={loading}
+// 				type="email"
+// 				name="email"
+// 				value={email}
+// 				onChange={(e) => {
+// 					setEmail(e.target.value);
+// 				}}
+// 			/>
+// 			<label htmlFor="password">Mot de passe</label>
+// 			<input
+// 				disabled={loading}
+// 				type="password"
+// 				name="password"
+// 				value={password}
+// 				onChange={(e) => {
+// 					setPassword(e.target.value);
+// 				}}
+// 			/>
+// 		</div>
+// 		<div className="modalAuth__submit">
+// 			<button onClick={handleSignup}>Se connecter</button>
+// 			<div className="modalAuth__submit__forgotPassword">
+// 				<a href="test"> Mot de passe oublié ? </a>
+// 			</div>
+// 		</div>
+// 	</div>
+// </div>
+// </div>
