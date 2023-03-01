b0e3dd2832f8ea8cdebc0c2f640ea6a4484624ac
AnaisRevellat <anaisrevellat@gmail.com>
2023-03-01 12:00:54 +0100

    testing push

--- src/components/ConnexionBtn/SignIn.tsx
+++ src/components/ConnexionBtn/SignIn.tsx
@@ -3,21 +3,21 @@ import { ModalAuth } from "../ModalAuth/ModalAuth";
 import "./SignIn.scss";
 
 function ConnexionBtn() {
-  const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-    <div className="button_wrapper_styles" >
-      <button className="connexionBtn" onClick={() => setIsOpen(true)} >
-        Connexion
-      </button>
-    </div>     
-
-      <ModalAuth open={isOpen} onClose={() => setIsOpen(false)} />
-   </>
-
-
-  );
+	return (
+		<>
+			<div className='button_wrapper_styles'>
+				<button
+					className='connexionBtn'
+					onClick={() => setIsOpen(true)}
+				>
+					Connexion
+				</button>
+			</div>
+			<ModalAuth open={isOpen} onClose={() => setIsOpen(false)} />
+		</>
+	);
 }
 
 export default ConnexionBtn;
