4fdd50174ea06a4506b1ed6b30fe487c4833df2a
AnaisRevellat <anaisrevellat@gmail.com>
2023-03-02 15:05:42 +0100

    test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Map from "./Maps/Map";
+import Map from "./maps/Map";
 import "./Home.scss";
 import DocData from "./Data.json";
 import { MainSearchBar } from "./SearchBar/MainSearchBar";
 
 function Home() {
-  return (
-    <>
-      <div className=""></div>
-      <div className="mainContainer">
-        <Map />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=''></div>
+			<div className='mainContainer'>
+				<Map />
+			</div>
+		</>
+	);
 }
 
 export default Home;
