f2c842218dea7b7c7845051d2f0bd7d3539a25a3
AnaisRevellat <anaisrevellat@gmail.com>
2023-04-25 11:54:01 +0200

    cleaning quickly front appearance

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -12,89 +12,93 @@ import Signin from "../ConnexionBtn/SignIn";
 import { useUser } from "../../context/UserContext";
 
 export const links = [
-  { label: "Accueil", path: "/" },
-  { label: "Villes", path: "/cities" },
-  { label: "Contact", path: "/contact" },
+	{ label: "Accueil", path: "/" },
+	{ label: "Villes", path: "/cities" },
+	{ label: "Contact", path: "/contact" },
 ];
 
 interface IProps {
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function NavBar({ ModalVisible, visible }: any) {
-  const [toggleMenu, setToggleMenu] = useState(true);
-  const [screenXWidth, setScreenXWidth] = useState(window.innerWidth);
+	const [toggleMenu, setToggleMenu] = useState(true);
+	const [screenXWidth, setScreenXWidth] = useState(window.innerWidth);
 
-  const { logout } = useUser();
+	const { logout } = useUser();
 
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenXWidth(window.innerWidth);
+	useEffect(() => {
+		const changeWidth = () => {
+			setScreenXWidth(window.innerWidth);
 
-      if (window.innerWidth > 1060) {
-        setToggleMenu(false);
-      }
-    };
+			if (window.innerWidth > 1060) {
+				setToggleMenu(false);
+			}
+		};
 
-    window.addEventListener("resize", changeWidth);
+		window.addEventListener("resize", changeWidth);
 
-    return () => {
-      //to avoid to listen to for nothing
-      window.removeEventListener("resize", changeWidth);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("resize", changeWidth);
+		};
+	}, []);
 
-  const handleShowModal = () => {
-    ModalVisible(!visible);
-  };
+	const handleShowModal = () => {
+		ModalVisible(!visible);
+	};
 
-  return (
-    <nav className="navBar">
-      {(toggleMenu || screenXWidth > 1060) && (
-        <div className="navBar__mainbox">
-          <Logo />
+	return (
+		<nav className='navBar'>
+			{(toggleMenu || screenXWidth > 1060) && (
+				<div className='navBar__mainbox'>
+					<Logo />
 
-          <ul className={"navBar__mainbox__list"}>
-            {links.map((link) => (
-              <CustomLink to={link.path} key={link.label}>
-                {link.label}
-              </CustomLink>
-            ))}
-          </ul>
-
-          {/* <div className='topBar'>
-						<SearchBar
-							className='navbarSearchbar'
-							placeholder='Entrez'
-							data={[]}
-						/>
-					</div> */}
-
-          <Signin />
-        </div>
-      )}
-      {window.innerWidth < 992 && (
-        <button className="navBar__burgerCross">X</button>
-      )}
-      <button onClick={logout}>logout</button>
-    </nav>
-  );
+					<ul className={"navBar__mainbox__list"}>
+						{links.map((link) => (
+							<CustomLink to={link.path} key={link.label}>
+								{link.label}
+							</CustomLink>
+						))}
+					</ul>
+					<div className="navBar__mainbox__connexionBtn">
+						<Signin />
+					</div>
+				</div>
+			)}
+			{window.innerWidth < 992 && (
+				<button
+					onClick={() => setToggleMenu(!toggleMenu)}
+					className='navBar__burgerCross'
+				>
+					X
+				</button>
+			)}
+			<button className="logoutBtn" onClick={logout}>logout</button>
+		</nav>
+	);
 }
 
-function CustomLink({ to, children, ...props }: { to: string; children: any }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({
-    path: resolvedPath.pathname,
-    end: true,
-  });
-  return (
-    <li
-      className={[isActive ? "active" : "", "items"].join(" ")}
-      data-testid={`listitem-link-${children}`}
-    >
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
+function CustomLink({
+	to,
+	children,
+	...props
+}: {
+	to: string;
+	children: any;
+}) {
+	const resolvedPath = useResolvedPath(to);
+	const isActive = useMatch({
+		path: resolvedPath.pathname,
+		end: true,
+	});
+	return (
+		<li
+			className={[isActive ? "active" : "", "items"].join(" ")}
+			data-testid={`listitem-link-${children}`}
+		>
+			<Link to={to} {...props}>
+				{children}
+			</Link>
+		</li>
+	);
 }
