5d0c243f0bd55e61f17b457d2e1acac26b7aafcf
AnaisRevellat <anaisrevellat@gmail.com>
2023-04-28 09:11:50 +0200

    css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import './footer.scss'
+import twitterLogo  from '../../assets/images/socialmedia/twitter.svg'
+import facebookLogo  from '../../assets/images/socialmedia/facebook.svg'
+import instagramLogo  from '../../assets/images/socialmedia/instagram.svg'
 
 const Footer = () => {
   return(
-    <div className="footer">
-      <h3>City Guide</h3>
-    </div>
+    <footer className="footer">      
+      <div className="footer__socialIcon">
+      <img src={facebookLogo} alt="logo twitter" />
+      <img src={twitterLogo} alt="logo twitter" />
+      <img src={instagramLogo} alt="logo twitter" />
+      </div>      
+    </footer>
   )
 }
 
--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./map.scss";
 import { gql, useQuery } from "@apollo/client";
@@ -7,62 +12,66 @@ import { getCities } from "../../graphql/city.server";
 import L from "leaflet";
 import { useNavigate } from "react-router-dom";
 
+//images
+import bicycle from "../../assets/images/bicycle/575bicycle_sagar_rana.png";
+import city from "../../assets/images/city/la_maison_rose_bastien_nvs555.png";
+
 export interface IData {
-  longitude: string;
-  latitude: string;
-  name: string;
-  id: number;
+	longitude: string;
+	latitude: string;
+	name: string;
+	id: number;
 }
 
 export interface IProps {
-  data: IData[];
+	data: IData[];
 }
 
 function Map() {
-  const { loading, error, data } = useQuery(getCities);
-  console.log(data, error, loading);
+	const { loading, error, data } = useQuery(getCities);
+	console.log(data, error, loading);
 
-  const navigate = useNavigate();
-  const handleMarkerClick = (city: IData) => {
-    navigate(`/city/${city.id}`, { state: { cityData: city } });
-  };
+	const navigate = useNavigate();
+	const handleMarkerClick = (city: IData) => {
+		navigate(`/city/${city.id}`, { state: { cityData: city } });
+	};
 
-  return (
-    <div className="mapDiv">
-      <MapContainer
-        className="mapDiv__container"
-        center={[46.083852, 2.508206]}
-        zoom={6}
-        scrollWheelZoom={false}
-        style={{ zIndex: 1 }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
+	return (
+		<div className='mapDiv'>
+			<MapContainer
+				className='mapDiv__container'
+				center={[46.083852, 2.508206]}
+				zoom={6}
+				scrollWheelZoom={false}
+				style={{ zIndex: 1 }}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+				/>
 
-        {data &&
-          data.Cities.map((city: IData) => {
-            return (
-              <Marker
-                key={city.id}
-                position={[
-                  parseFloat(city.latitude),
-                  parseFloat(city.longitude),
-                ]}
-                eventHandlers={{
-                  click: () => {
-                    handleMarkerClick(city);
-                  },
-                }}
-              >
-                {/* <Popup>{city.name}</Popup> */}
-              </Marker>
-            );
-          })}
-      </MapContainer>
-    </div>
-  );
+				{data &&
+					data.Cities.map((city: IData) => {
+						return (
+							<Marker
+								key={city.id}
+								position={[
+									parseFloat(city.latitude),
+									parseFloat(city.longitude),
+								]}
+								eventHandlers={{
+									click: () => {
+										handleMarkerClick(city);
+									},
+								}}
+							>
+								{/* <Popup>{city.name}</Popup> */}
+							</Marker>
+						);
+					})}
+			</MapContainer>
+		</div>
+	);
 }
 
 export default Map;
